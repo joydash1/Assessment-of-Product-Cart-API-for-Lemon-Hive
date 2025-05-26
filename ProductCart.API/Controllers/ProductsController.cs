9a770b29d2e5e99cf89cc037c1c32bd845aa5ad7
Nir jhor <nirjhordas2020@gmail.com>
2025-05-26 17:10:38 +0600

    Done

--- ProductCart.API/Controllers/ProductsController.cs
+++ ProductCart.API/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductCart.Infrastructure.Domains;
 using ProductCart.Interfaces;
+using ProductCart.Services;
 
 namespace ProductCart.API.Controllers
 {
@@ -25,5 +27,50 @@ namespace ProductCart.API.Controllers
                 Data = productList
             });
         }
+        [HttpPost]
+        [Route("SaveProduct")]
+        public async Task<IActionResult> SaveProductsAsync([FromForm] Product model)
+        {
+            try
+            {
+
+                if (model.File != null)
+                {
+                    var fileDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Product Images");
+
+                    if (!Directory.Exists(fileDirectory))
+                    {
+                        Directory.CreateDirectory(fileDirectory);
+                    }
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + model.File.FileName;
+                    string filePath = Path.Combine(fileDirectory, uniqueFileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await model.File.CopyToAsync(stream);
+                    }
+
+                    model.ProductImage = uniqueFileName;
+                    model.ProductImage = Path.Combine("Product Images", uniqueFileName);
+                }
+
+                await _context.ProductRepository.AddAsync(model);
+                await _context.CommitAsync();
+                
+                return Ok(new
+                {
+                    Status = true,
+                    Message = "Product Save Successfully."
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Status = false,
+                    Message = "There is something wrong. please try again."
+                });
+            }
+        }
+
     }
 }
--- ProductCart.Infrastructure/Domains/Product.cs
+++ ProductCart.Infrastructure/Domains/Product.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +13,15 @@ namespace ProductCart.Infrastructure.Domains
     {
         [Key]
         public int Id { get; set; }
+        public required string ProductName { get; set; }
+        public required decimal ProductPrice { get; set; }
+        public required string ProductSlug { get; set; }
+        public string? ProductImage { get; set; }
 
-        public string ProductName { get; set; }
-        public decimal ProductPrice { get; set; }
-        public string ProductSlug { get; set; }
         public DateTime? DiscountStartDate { get; set; }
         public DateTime? DiscountEndDate { get; set; }
+
+        [NotMapped]
+        public IFormFile? File { get; set; }
     }
 }
